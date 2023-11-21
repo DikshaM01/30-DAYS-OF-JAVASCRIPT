eefec0250d23a49260a7cc65392ffc84324fba62
Diksha  Manu <127192504+DikshaM01@users.noreply.github.com>
2023-11-21 07:37:15 +0000

    add index.js to compute to calculate the sum of two number

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let num1=process.argv[2]
+let num2=process.argv[3]
+let result=num1+num2;
+let num3=parseInt(process.argv[2])
+let num4=parseInt(process.argv[3])
+let ans=num3+num4
+console.log(ans)
