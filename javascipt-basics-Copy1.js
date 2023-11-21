db51ceb32dac6b28b737bc3de4ebc0cfe5339b00
Diksha  Manu <127192504+DikshaM01@users.noreply.github.com>
2023-11-21 09:26:56 +0530

    Add files via upload

---
+++ javascipt-basics-Copy1.js
@@ -0,0 +1,210 @@
+console.log('Hello World')
+
+console.log(2+3)
+
+console.log(5*10)
+
+console.log(10**2) //power
+
+let firstname='john'
+
+firstname
+
+firstname='aman'
+
+firstname
+
+let x=2;
+let y=3;
+let result=x+y
+result
+
+let answer=42 //number
+let pi=3.14 //float
+
+answer
+
+
+pi
+
+let greetings='Hello,World';
+
+greetings
+
+let selected=true;
+let failed=false;
+
+selected
+
+let valuess;
+
+valuess //this is undefined
+
+let str="2";
+
+typeof str;
+
+let a=2;
+let b=3;
+a>b
+
+b<a
+
+b>a
+
+3=="3"
+
+3==='3' //strictly comparison
+
+let numc=1;
+numc>0 && numc<=1
+
+num==1 ||num>2
+
+!(num==2)
+
+let namess='Diksha'
+
+namess.length;
+
+namess.charAt(0);
+
+let strs="Hello";
+let strs1="World";
+let ans1=strs+strs1;
+ans1
+
+namess.toUpperCase()
+
+namess.toLowerCase()
+
+namess.indexOf('d')
+
+namess.indexOf('i')
+
+namess
+
+namess.indexOf('D')
+
+namess.substring(2,5)
+
+let names2='INDIA'
+
+names2.replace("INDIA","BHARAT")
+
+let colours=['red','green','blue']
+let mixed=['red',1,true,[true,false]]
+let empty=[]
+
+colours[0]
+
+mixed[3]
+
+mixed.length
+
+colours.push("orange")
+
+colours
+
+colours.pop()
+
+colours
+
+let fruits=['apple']
+let morefruits=['orange','grape']
+let all=fruits.concat(morefruits)
+all
+
+let index=fruits.indexOf('orange')
+
+let sliced=fruits.slice(1,3)
+
+sliced
+
+3/0
+
+let person={name:"John Doe",age:30,gender:"Male"}
+
+person.name;
+
+person["age"]
+
+
+
+
+Object.values(person);
+
+
+
+
+
+
+Object.keys(person);
+
+
+
+
+
+
+let personv = { name: "John", age: 30, gender: "Male" };
+
+Object.entries(person);
+
+
+
+
+person.hasOwnProperty("name"); // Output: true
+person.hasOwnProperty("title"); // Output: false
+
+let n=5
+let results;
+if(n>0)
+{
+results="positive"
+}
+results
+
+let sum=0
+for(let i=0;i<=5;i++)
+{
+sum=sum+i;
+}
+sum
+
+let add1=1;
+let counter1=6;
+while(counter1>0)
+
+
+{
+add1=add1+counter1;
+counter1=counter1-1;
+}
+add1
+
+let nn112=[1,2,3]
+let sumsn12=0;
+for (let sumsn22 in nn112)
+{
+sumsn12=sumsn12+sumsn22
+}
+sumsn12
+
+function calculatearea(widthss,heightss)
+{
+let areass=widthss*heightss
+return areass
+}
+let ansss=calculatearea(2,4)
+ansss
+
+calculatearea(2,4)
+
+function calculateTotalPrice(item) {
+  let totalPrice = item.price * item.quantity;
+  "Total price is: " + totalPrice;
+}
+
+calculateTotalPrice({ name: "Shirt", price: 20, quantity: 2 });
+
+
