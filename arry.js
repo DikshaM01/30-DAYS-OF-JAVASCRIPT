4fd0c2b97a137a45bc94e8115aef36de10f24f42
Diksha  Manu <127192504+DikshaM01@users.noreply.github.com>
2023-11-24 08:27:22 +0000

    array

---
+++ arry.js
@@ -0,0 +1,6 @@
+//single variable that stores multiple elements
+var house=['1BHK','2BHK'];//method1
+var numbers=new Array(10,20,30,40);//method 2
+var mixed=['1BHK',20,true];
+console.log(mixed[0]+"my house");
+//using every method,map,filter,reduce,some
