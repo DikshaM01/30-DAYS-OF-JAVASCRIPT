d0f0955476f1a760fe16128b462c05977314677e
Diksha  Manu <127192504+DikshaM01@users.noreply.github.com>
2023-11-24 03:21:24 +0000

    arrow function

---
+++ arrow.js
@@ -0,0 +1,20 @@
+var greet=() =>
+{
+    console.log('hello');
+}
+greet();
+//normally in arrow function we use const keyword
+ 
+function addi()
+{
+    let a=5;
+    let b=10;
+    return a+b;
+}
+console.log(addi());
+
+function mul(num)
+{
+    return num*num;
+}
+console.log(mul(4));
