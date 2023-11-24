dbfe71d6961a9f5b4903cbe3a2b3a1ca8b81ff97
Diksha  Manu <127192504+DikshaM01@users.noreply.github.com>
2023-11-24 02:58:37 +0000

    function in js

---
+++ functioninjs.js
@@ -0,0 +1,21 @@
+//alert() is a basic built-in function
+function add(num1,num2)
+{
+   console.log(num1+num2)
+}
+let first=12
+let second=13
+add(first,second)
+
+function print(name)
+{
+    console.log(name)
+}
+print('Diksha')
+print('manu')
+
+//anonymous function
+let anonymous=Function()
+{
+console.log('hello')
+}
