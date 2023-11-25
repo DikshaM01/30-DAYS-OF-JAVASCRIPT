a5fb94705df39c34174716a5b2d410a4b02d6c9a
Diksha  Manu <127192504+DikshaM01@users.noreply.github.com>
2023-11-25 05:24:23 +0000

    event

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.getElementById(element_id);//search
+element.querySelctorAll(selectors)//css selector
+element.addEventListener(event,listener,useCapture);
+addEventListener(type,listener,useCapture)
